fdbe525f5b3fa48426d05894841b88d6d22f789a
abruck1 <agbruck@gmail.com>
2017-09-25 01:12:34 -0500

    fixed the polls website

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import datetime
-from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
+import datetime
 
-
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -18,7 +18,7 @@ class Question(models.Model):
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
