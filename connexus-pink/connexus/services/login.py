2a9fc7d943f06c4a99a5614e2e109fb3c226f166
p3ntak <wey.matt@gmail.com>
2017-10-01 17:40:18 -0500

    added some test methods for sending emails but cannot get a required import to work

--- connexus-pink/connexus/services/login.py
+++ connexus-pink/connexus/services/login.py
@@ -5,6 +5,7 @@ import jinja2
 import webapp2
 
 from connexus.common import *
+# from webapp2_extras import auth
 # from google.oauth2 import id_token
 # from google.auth.transport import requests
 
@@ -14,7 +15,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # google auth needs this
-CLIENT_ID = "726264805787-r04pldtjtkr88b255pfu802do7h6b0r0.apps.googleusercontent.com"
+# CLIENT_ID = "726264805787-r04pldtjtkr88b255pfu802do7h6b0r0.apps.googleusercontent.com"
 
 
 # [START home_page]
@@ -22,9 +23,13 @@ class Login(webapp2.RequestHandler):
 
     def get(self):
 
+        user = users.get_current_user()
+
+        if user:
+            self.redirect("/view")
+
         template_values = {
             'page': "Connex.us",
-            'CLIENT_ID': CLIENT_ID,
         }
         url, url_linktext, user = logout_func(self, user_text='Enter Connex.us!')
         template_values['url'] = url
@@ -34,31 +39,12 @@ class Login(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('login.html')
         self.response.write(template.render(template_values))
 
-    def post(self):
-
-        token = self.request.get("idtoken")
-        print("id_token {}".format(token))
-
-        try:
-            idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-
-            # Or, if multiple clients access the backend server:
-            # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-            # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-            #     raise ValueError('Could not verify audience.')
-
-            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-                raise ValueError('Wrong issuer.')
-
-            # If auth request is from a G Suite domain:
-            # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-            #     raise ValueError('Wrong hosted domain.')
-
-            # ID token is valid. Get the user's Google Account ID from the decoded token.
-            userid = idinfo['sub']
-            print(userid)
-        except ValueError:
-            # Invalid token
-            pass
+    # def post(self):
+    #
+    #     user_email = self.request.get("idtoken")
+    #     print("id_token 1 {}".format(user_email))
+    #
+    #     # auth.AuthStore.create_auth_token(user_email)
+    #     self.redirect('/view?user=' + user_email)
 
 # [END home_page]
