69d7734fe7381130524d25ff113dd90f502ca285
abruck1 <agbruck@gmail.com>
2017-10-02 22:25:45 -0500

    search seems to work

--- connexus-pink/connexus/services/search.py
+++ connexus-pink/connexus/services/search.py
@@ -7,6 +7,7 @@ from google.appengine.api import search
 import urllib2
 
 from connexus.common import *
+from connexus.ndb_model import *
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.PackageLoader('connexus', 'templates'),
@@ -27,21 +28,39 @@ class Search(webapp2.RequestHandler):
         search_string = urllib2.unquote(self.request.query_string)
 
         index = search.Index(name='connexus_search')
+        num_results = 0
+        found_streams = []
         if search_string:
             try:
                 index = search.Index('connexus_search')
+                streams = Stream.query()
+                for s in streams:
+                    key = str(s.key)
+                    print("s={0} key={1}".format(s, key))
+                    doc = search.Document(doc_id=str(s.key.id()),
+                                          fields=[search.TextField(name='name', value=s.name)]
+                                         )
+                    search.Index(name='connexus_search').put(doc)
+
                 search_query = search.Query(query_string=search_string)
                 search_results = index.search(search_query)
                 print("Search Results={}".format(search_results))
+                num_results = search_results.number_found
+                for doc in search_results:
+                    doc_id = doc.doc_id
+                    found_streams.append(ndb.Key(Stream, int(doc_id)).get())
+                    fields = doc.fields
+                    print("doc_id={0} fields={1}".format(doc_id, fields)) 
+            
             except search.Error:
                 print("Caught a search Error")
 
 
-        num_results = 0
         template_values = {
             'page': "Search",
             'search_string': search_string,
             'num_results': num_results,
+            'found_streams': found_streams,
         }
         url, url_linktext, user = logout_func(self)
         template_values['url'] = url
