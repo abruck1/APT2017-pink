95f9ae8d61c6af442d4537775435a002efaf46e8
jtautry <jtautry@gmail.com>
2017-10-12 16:21:09 -0500

    added content type to response

--- connexus-pink/connexus/services/get_upload_url.py
+++ connexus-pink/connexus/services/get_upload_url.py
@@ -4,5 +4,6 @@ from google.appengine.ext import blobstore
 
 class GetUploadURL(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(blobstore.create_upload_url('/upload', ))
         # upload_url = blobstore.create_upload_url('/upload', )
